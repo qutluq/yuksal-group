3fc43be92133d8031c859a6d97459a5c43392497
qutluq <kootlook.applications@gmail.com>
2023-08-09 17:41:39 +0600

    feat: utilities

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,4 +1,9 @@
+import { count } from 'console'
+import { cache } from 'react'
+
+import page from '@/app/(site)/page'
 import { PrismaClient } from '@prisma/client'
+
 // Instantiate a single instance PrismaClient and save it on the globalThis object.
 // Then we keep a check to only instantiate PrismaClient if it's not on the globalThis object otherwise use the same
 // instance again if already present to prevent instantiating extra PrismaClient instances.
@@ -13,3 +18,45 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export const getPosts = cache(async ({ page, limit }) => {
+  const count = await prisma.post.count({
+    where: {
+      published: true,
+    },
+  })
+
+  const results = await prisma.post.findMany({
+    skip: (page - 1) * limit, // How many rows to skip
+    take: limit, // Page size
+    orderBy: {
+      publishedAt: { sort: 'desc', nulls: 'last' },
+    },
+    where: {
+      published: true,
+    },
+  })
+
+  return { posts: results, total: count }
+})
+
+export const getPost = cache(async (slug: string) => {
+  const result = await prisma.post.findMany({
+    take: 1,
+    where: {
+      slug,
+    },
+  })
+
+  return result
+})
+
+export const getAuthor = cache(async (id: number) => {
+  const result = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  return result
+})
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,41 @@
 export const classNames = (...classes: (string | boolean)[]) =>
   classes.filter(Boolean).join(' ')
+
+export const getPaginationText = (page, limit, total, short = false) =>
+  short
+    ? `${total > 0 ? (page - 1) * limit + 1 : 0}/${
+        page * limit > total ? total : page * limit
+      } of ${total} `
+    : `Showing ${total > 0 ? (page - 1) * limit + 1 : 0} to ${
+        page * limit > total ? total : page * limit
+      } of ${total} `
+
+export const formatDate = (date: Date | null) => {
+  if (date === null) {
+    return 'n/a'
+  }
+
+  type OptionsType = {
+    year: 'numeric' | '2-digit' | undefined
+    month: 'numeric' | '2-digit' | 'short' | undefined
+    day: 'numeric' | '2-digit' | undefined
+  }
+
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  } as OptionsType
+
+  return new Intl.DateTimeFormat('en-US', options).format(date)
+}
+
+export const slugify = (str: string) =>
+  String(str)
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
