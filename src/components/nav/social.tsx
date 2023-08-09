e07d80865e30393965e9e4dbad206a1af92a4843
qutluq <kootlook.applications@gmail.com>
2023-08-09 17:51:32 +0600

    fix:social links not opening

--- src/components/nav/social.tsx
+++ src/components/nav/social.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link'
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa6'
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa6'
 
 export const SocialLinks = () => (
   <div className="flex flex-row justify-right">
     <Link
-      href="wwww.facebook.com"
+      href="https://wwww.facebook.com"
+      target="_blank"
       className="hover:bg-zinc-500 p-2 hover:rounded-md hover:bg-opacity-40"
     >
       <FaFacebookF className="text-gray-50" />
     </Link>
     <Link
-      href="wwww.twitter.com"
+      href="https://wwww.twitter.com"
+      target="_blank"
       className="hover:bg-zinc-500 p-2 hover:rounded-md hover:bg-opacity-40"
     >
       <FaTwitter className="text-gray-50" />
     </Link>
     <Link
-      href="wwww.instagram.com"
+      href="https://wwww.instagram.com"
+      target="_blank"
       className="hover:bg-zinc-500 p-2 hover:rounded-md hover:bg-opacity-40"
     >
       <FaInstagram className="text-gray-50" />
