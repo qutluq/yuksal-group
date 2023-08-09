85d17cb90a97992496d8667e2ce72b64d0f8f3fa
qutluq <kootlook.applications@gmail.com>
2023-08-09 17:35:38 +0600

    feat: updated seeding algorithm

--- prisma/seeds/seedBlog.ts
+++ prisma/seeds/seedBlog.ts
@@ -1,14 +1,28 @@
+import { slugify } from '@/utils'
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
-import seedUsers from './users'
 
 type BlogPostType = {
   title: string
+  slug: string
   content: string
+  description: string
+  readingTime: number
   authorId: number
   published: boolean
-  publishedAt: Date
+  publishedAt: Date | null
   updatedAt: Date
+}
+
+const loremParagraphs = (min: number, max: number, separator = '\n') => {
+  const n = faker.number.int({ min, max })
+  let str = ''
+  for (let i = 0; i < n; i++) {
+    str = str.concat(faker.lorem.sentences({ min: 5, max: 17 }))
+    str = str.concat(separator)
+  }
+
+  return str
 }
 
 const seedBlog = async (count: number = 11) => {
@@ -25,14 +39,38 @@ const seedBlog = async (count: number = 11) => {
       try {
         const posts: BlogPostType[] = []
 
+        const dates = faker.date
+          .betweens({
+            from: '2020-01-01T00:00:00.000Z',
+            to: '2023-01-01T00:00:00.000Z',
+            count: count,
+          })
+          .sort((a, b) => a.getTime() - b.getTime())
+
+        const slugs = ['']
+
         for (let i = 0; i < count; i++) {
+          let title = ''
+          let slug = ''
+          while (true) {
+            //make sure slugs are unique
+            title = faker.commerce.productName()
+            slug = slugify(title)
+            if (slugs.indexOf(slug) == -1) {
+              break
+            }
+          }
+          const published = faker.number.int({ min: 1, max: 7 }) !== 3
           posts.push({
-            title: faker.commerce.productName(),
-            content: faker.lorem.paragraph({ min: 5, max: 11 }),
+            title: title,
+            slug: slugify(title),
+            content: loremParagraphs(10, 17),
+            description: faker.lorem.sentences({ min: 3, max: 5 }),
+            readingTime: faker.number.int({ min: 1, max: 7 }),
             authorId: user.id,
-            published: true,
-            publishedAt: new Date(),
-            updatedAt: new Date(),
+            published: published,
+            publishedAt: published ? dates[i] : null,
+            updatedAt: dates[i],
           })
         }
 
