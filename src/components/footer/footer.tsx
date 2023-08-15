eb5833928e8ee56e711c5da2eb4ecce21ae6dcbe
qutluq <kootlook.applications@gmail.com>
2023-08-15 23:05:38 +0600

    style: footer wrap text on small screens

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,15 +8,18 @@ export const Footer = () => {
   const { i18n } = useLingui()
 
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-white/10 py-3 text-sm text-[var(--color-text-primary)] sm:flex-row md:text-lg">
-      <p>Yuksal Group © 2023 \ {t`All Rights Reserved`} \&nbsp;</p>
-      <div className="flex flex-row gap-2">
-        <p>{t`Design by`}</p>
+    <div className="flex w-full flex-col items-center justify-center gap-1 bg-white/10 py-3 text-sm text-[var(--color-text-primary)] sm:flex-row sm:gap-2 md:text-lg">
+      <p>Yuksal Group © 2023</p>
+      <p className="hidden sm:inline-block">{' / '}</p>
+      <p>{t`All Rights Reserved`}</p>
+      <p className="hidden sm:inline-block">{' / '}</p>
+      <p className="flex flex-row gap-2">
+        {t`Design by`}
         <Link
           className="text-[var(--color-primary)]"
           href="https://github.com/qutluq"
         >{t`Qutluq`}</Link>
-      </div>
+      </p>
     </div>
   )
 }
