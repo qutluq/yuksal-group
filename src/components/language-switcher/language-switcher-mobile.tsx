7d60402deaaa98f96342f0ae48c31cab3ce2ad5a
qutluq <kootlook.applications@gmail.com>
2023-08-20 12:34:10 +0600

    feat: connect lang-switcher to cookie locale

--- src/components/language-switcher/language-switcher-mobile.tsx
+++ src/components/language-switcher/language-switcher-mobile.tsx
@@ -1,15 +1,33 @@
-import { localeLanguages, locales } from '@/locales'
-import { localeChanged } from '@/redux/features/locale/locale-slice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { classNames } from '@/utils'
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import { localeLanguages } from '@/locales'
+import { classNames, getDefaultLocale, setSearchParamsLang } from '@/utils'
+
+import nextConfig from '../../../next.config'
 
 export const LanguageSwitcherMobile = () => {
-  const locale = useAppSelector((state) => state.localeManager.locale)
-  const dispatch = useAppDispatch()
+  const router = useRouter()
+  const [selectedLang, setSelectedLang] = useState(
+    getDefaultLocale({ env: 'client' }),
+  )
 
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const locales = nextConfig.i18n?.locales || ['en']
   const selectLanguage = (code: string) => {
-    dispatch(localeChanged(code))
+    setSelectedLang(code)
   }
+
+  useEffect(() => {
+    document.cookie = `NEXT_LOCALE=${selectedLang}; max-age=${
+      2 * (3 * 365 + 366) * 7 * 24 * 60 * 60
+    }; path=/`
+    const query = setSearchParamsLang(searchParams, selectedLang)
+    router.push(`${pathname}${query}`)
+  }, [selectedLang])
 
   return (
     <div className="flex flex-col text-white">
@@ -19,7 +37,7 @@ export const LanguageSwitcherMobile = () => {
           onClick={() => selectLanguage(code)}
           className={classNames(
             'px-2 py-3 text-sm font-semibold   uppercase text-gray-50 hover:rounded-md hover:bg-opacity-40',
-            locale === code && 'underline underline-offset-4',
+            selectedLang === code && 'underline underline-offset-4',
           )}
         >
           {localeLanguages[code]}
--- src/components/language-switcher/language-switcher.tsx
+++ src/components/language-switcher/language-switcher.tsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-import { localeLanguages, locales } from '@/locales'
-import { localeChanged } from '@/redux/features/locale/locale-slice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { localeLanguages } from '@/locales'
+import { getDefaultLocale, setSearchParamsLang } from '@/utils'
+
+import nextConfig from '../../../next.config'
 
 export const LanguageSwitcher = () => {
   const [expanded, setExpanded] = useState(false)
+  const router = useRouter()
+  const [selectedLang, setSelectedLang] = useState(
+    getDefaultLocale({ env: 'client' }),
+  )
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const locale = getDefaultLocale({ env: 'client' })
 
-  const locale = useAppSelector((state) => state.localeManager.locale)
-  const dispatch = useAppDispatch()
+  const locales = nextConfig.i18n?.locales || ['en']
 
   const selectLanguage = (code: string) => {
+    setSelectedLang(code)
     setExpanded(false)
-    dispatch(localeChanged(code))
   }
+
+  useEffect(() => {
+    document.cookie = `NEXT_LOCALE=${selectedLang}; max-age=${
+      2 * (3 * 365 + 366) * 7 * 24 * 60 * 60
+    }; path=/`
+    const query = setSearchParamsLang(searchParams, selectedLang)
+    router.push(`${pathname}${query}`)
+  }, [selectedLang])
 
   if (expanded) {
     return (
