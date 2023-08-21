3a23a9d4b2b37e938ef38e9cf267e6de0ccfa0f5
qutluq <kootlook.applications@gmail.com>
2023-08-21 19:17:32 +0600

    chore: cleanup

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,6 @@ import { getAllTranslations, getTranslationFunction } from '@/utils/db'
 export const Footer = async ({ lang }: { lang: string }) => {
   const translations = await getAllTranslations()
   const translate = getTranslationFunction(translations, lang)
-  console.log({ tr: translate('All Rights Reserved'), lang })
   return (
     <div className="flex w-full flex-col items-center justify-center gap-1 bg-white/10 py-3 text-sm text-[var(--color-text-primary)] sm:flex-row sm:gap-2 md:text-lg">
       <p>Yuksal Group © 2023</p>
