2787ebda06f99f4d1f7b268687d179a140558324
qutluq <kootlook.applications@gmail.com>
2023-08-31 20:04:33 +0600

    fix: nav max-width w-full instead of fixed

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -44,133 +44,135 @@ export const Nav = ({ navItems, navItemsMobile, lang }: PropTypes) => {
   return (
     <div
       className={classNames(
-        'fixed z-10 flex h-[76px] w-full max-w-7xl flex-row items-start justify-between bg-[var(--color-secondary)] px-5 pt-5',
+        'fixed z-10 flex h-[76px] w-full flex-row justify-center bg-[var(--color-secondary)]',
         colorChange ? 'border-b border-white' : 'md:bg-transparent',
       )}
     >
-      <div className="md:hidden">
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button>
-                <IoMenuOutline
-                  className={classNames(
-                    'text-3xl text-white',
-                    open && 'hidden',
-                  )}
-                />
-                <TfiClose
-                  className={classNames(
-                    'text-3xl text-white',
-                    !open && 'hidden',
-                  )}
-                />
-              </Disclosure.Button>
-
-              <nav
-                className={classNames(
-                  'fixed left-0 top-[76px] z-10 flex flex-col p-2 md:hidden',
-                  open &&
-                    'h-screen w-[270px] animate-[translate_0.2s_ease-in-out] bg-[var(--color-secondary)]',
-                )}
-              >
-                <Disclosure.Panel className="flex w-auto flex-col md:hidden">
-                  {navItemsMobile}
-                  <div className="border-t">
-                    <LanguageSwitcherMobile lang={lang} />
-                  </div>
-                </Disclosure.Panel>
-              </nav>
-            </>
-          )}
-        </Disclosure>
-      </div>
-      <Link href={'/'}>
-        <Image src="/img/logo.png" alt="Logo" width={192} height={32} />
-      </Link>
-
-      <nav className=" hidden gap-1 tracking-widest md:flex lg:gap-3">
-        {navItems}
-      </nav>
-
-      <div className="hidden md:block md:w-2 lg:hidden"> </div>
-
-      {session && (
-        <div className="flex-row gap-7 lg:flex">
-          <div className="w-8 lg:hidden"> </div>
-          <div className="right-20 hidden pt-[6px] md:fixed md:block lg:static">
-            <LanguageSwitcher lang={lang} />
-          </div>
-
+      <div className="flex w-full max-w-7xl flex-row items-start justify-between px-5 pt-5">
+        <div className="md:hidden">
           <Disclosure>
-            <div className="fixed right-5 lg:static">
-              <Disclosure.Button>
-                <Image
-                  src={DEFAULT_AUTHOR_IMG}
-                  alt=""
-                  className="rounded-full object-cover"
-                  width={35}
-                  height={35}
-                />
-              </Disclosure.Button>
-            </div>
-            <div
-              className={classNames(
-                'fixed right-5',
-                !colorChange && 'top-[80px] md:top-[70px]',
-                colorChange && 'top-[80px] md:top-[86px]',
-              )}
-            >
-              <Disclosure.Panel className={'flex flex-col items-start gap-3'}>
-                <p>{session?.user?.name}</p>
-                <div className="flex flex-col justify-end">
-                  <SocialLinks variant="submenu" />
-                </div>
-                <div className="block border-t md:hidden">
-                  <LanguageSwitcherMobile lang={lang} />
-                </div>
-                <div className="flex w-full flex-row justify-center">
-                  <SingOutButton title={translate('Sign out', lang)} />
-                </div>
-              </Disclosure.Panel>
-            </div>
-          </Disclosure>
-        </div>
-      )}
+            {({ open }) => (
+              <>
+                <Disclosure.Button>
+                  <IoMenuOutline
+                    className={classNames(
+                      'text-3xl text-white',
+                      open && 'hidden',
+                    )}
+                  />
+                  <TfiClose
+                    className={classNames(
+                      'text-3xl text-white',
+                      !open && 'hidden',
+                    )}
+                  />
+                </Disclosure.Button>
 
-      {!session && (
-        <div className="flex flex-row gap-3">
-          <div className="hidden flex-row lg:flex">
-            <SocialLinks />
-          </div>
-          <div className="right-12 hidden pt-[6px] md:fixed md:block lg:static">
-            <LanguageSwitcher lang={lang} />
-          </div>
-          <div className="pt-[9px] lg:hidden">
-            <Disclosure>
-              <Disclosure.Button className={'right-6 z-10 md:fixed'}>
-                <PiDotsThreeOutlineVerticalFill />
-              </Disclosure.Button>
-
-              <div
-                className={classNames(
-                  'fixed right-1 pt-7',
-                  !colorChange && 'md:top-17',
-                  colorChange && 'md:top-14',
-                )}
-              >
-                <div className="flex flex-col items-end">
-                  <Disclosure.Panel>
-                    <div className="flex flex-col justify-end">
-                      <SocialLinks variant="submenu" />
+                <nav
+                  className={classNames(
+                    'fixed left-0 top-[76px] z-10 flex flex-col p-2 md:hidden',
+                    open &&
+                      'h-screen w-[270px] animate-[translate_0.2s_ease-in-out] bg-[var(--color-secondary)]',
+                  )}
+                >
+                  <Disclosure.Panel className="flex w-auto flex-col md:hidden">
+                    {navItemsMobile}
+                    <div className="border-t">
+                      <LanguageSwitcherMobile lang={lang} />
                     </div>
                   </Disclosure.Panel>
-                </div>
+                </nav>
+              </>
+            )}
+          </Disclosure>
+        </div>
+        <Link href={'/'}>
+          <Image src="/img/logo.png" alt="Logo" width={192} height={32} />
+        </Link>
+
+        <nav className=" hidden gap-1 tracking-widest md:flex lg:gap-3">
+          {navItems}
+        </nav>
+
+        <div className="hidden md:block md:w-2 lg:hidden"> </div>
+
+        {session && (
+          <div className="flex-row gap-7 lg:flex">
+            <div className="w-8 lg:hidden"> </div>
+            <div className="right-20 hidden pt-[6px] md:fixed md:block lg:static">
+              <LanguageSwitcher lang={lang} />
+            </div>
+
+            <Disclosure>
+              <div className="fixed right-5 lg:static">
+                <Disclosure.Button>
+                  <Image
+                    src={DEFAULT_AUTHOR_IMG}
+                    alt=""
+                    className="rounded-full object-cover"
+                    width={35}
+                    height={35}
+                  />
+                </Disclosure.Button>
+              </div>
+              <div
+                className={classNames(
+                  'fixed right-5',
+                  !colorChange && 'top-[80px] md:top-[70px]',
+                  colorChange && 'top-[80px] md:top-[86px]',
+                )}
+              >
+                <Disclosure.Panel className={'flex flex-col items-start gap-3'}>
+                  <p>{session?.user?.name}</p>
+                  <div className="flex flex-col justify-end">
+                    <SocialLinks variant="submenu" />
+                  </div>
+                  <div className="block border-t md:hidden">
+                    <LanguageSwitcherMobile lang={lang} />
+                  </div>
+                  <div className="flex w-full flex-row justify-center">
+                    <SingOutButton title={translate('Sign out', lang)} />
+                  </div>
+                </Disclosure.Panel>
               </div>
             </Disclosure>
           </div>
-        </div>
-      )}
+        )}
+
+        {!session && (
+          <div className="flex flex-row gap-3">
+            <div className="hidden flex-row lg:flex">
+              <SocialLinks />
+            </div>
+            <div className="right-12 hidden pt-[6px] md:fixed md:block lg:static">
+              <LanguageSwitcher lang={lang} />
+            </div>
+            <div className="pt-[9px] lg:hidden">
+              <Disclosure>
+                <Disclosure.Button className={'right-6 z-10 md:fixed'}>
+                  <PiDotsThreeOutlineVerticalFill />
+                </Disclosure.Button>
+
+                <div
+                  className={classNames(
+                    'fixed right-1 pt-7',
+                    !colorChange && 'md:top-17',
+                    colorChange && 'md:top-14',
+                  )}
+                >
+                  <div className="flex flex-col items-end">
+                    <Disclosure.Panel>
+                      <div className="flex flex-col justify-end">
+                        <SocialLinks variant="submenu" />
+                      </div>
+                    </Disclosure.Panel>
+                  </div>
+                </div>
+              </Disclosure>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
