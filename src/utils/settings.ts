9bac2d4737dabbbeb844c409d04f48143c17dbd5
qutluq <kootlook.applications@gmail.com>
2023-09-18 15:52:18 +0600

    refactor: move nav settings to nav component dir

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -10,10 +10,3 @@ export const LOGO_IMG = '/assets/logo.png' //aspect ratio: 200:36
 export const SiteName = 'Yuksal Group'
 export const SiteDescription = 'Uyghur Musical Group: Naghra/Surney/Karnay'
 export const SiteUrl = 'https://www.yuksal-group.com'
-export const NAV_MENU = () => [
-  { id: 'home', name: 'home', slug: 'home' },
-  { id: 'blog', name: 'blog', slug: 'blog' },
-  { id: 'about', name: 'about', slug: 'about' },
-  { id: 'gallery', name: 'gallery', slug: 'gallery' },
-  { id: 'contact', name: 'contact', slug: 'contact' },
-]
