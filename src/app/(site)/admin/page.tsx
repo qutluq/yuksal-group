d73ed4d61e4ea9060223e6532ea7d4c938cb3ec5
qutluq <kootlook.applications@gmail.com>
2023-09-24 21:44:01 +0600

    feat: redirect admin page to admin/blog

--- src/app/(site)/admin/page.tsx
+++ src/app/(site)/admin/page.tsx
@@ -1,40 +1,7 @@
-import type { Metadata } from 'next'
-
-import { Admin as AdminComponent } from '@/components/admin'
-import { AdminLayout } from '@/layouts/admin'
-import {
-  getLangSearchParam,
-  getMetadata,
-  toTitleCase,
-  translate,
-} from '@/utils'
+import { redirect } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
 
-export async function generateMetadata({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}): Promise<Metadata> {
-  const lang = getLangSearchParam(searchParams)
-  const title = toTitleCase(translate('admin', lang))
-
-  return getMetadata({ title })
-}
-
-const Admin = ({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) => {
-  const lang = getLangSearchParam(searchParams)
-  return (
-    <div className="text-[var(--color-text-primary)]">
-      <AdminLayout lang={lang}>
-        <AdminComponent lang={lang} />
-      </AdminLayout>
-    </div>
-  )
-}
+const Admin = () => redirect('/admin/blog')
 
 export default Admin
