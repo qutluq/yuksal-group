c9d0dd5f35955471bf178d4678fb72c308e812f9
qutluq <kootlook.applications@gmail.com>
2023-09-24 22:16:57 +0600

    feat: admin menu

--- src/components/nav/nav-items.tsx
+++ src/components/nav/nav-items.tsx
@@ -1,5 +1,5 @@
 import { NavItem } from './nav-item'
-import { NAV_MENU } from './settings'
+import { NAV_MENU, NAV_MENU_ADMIN } from './settings'
 
 type PropTypes = {
   page: string
@@ -7,7 +7,19 @@ type PropTypes = {
   mode?: 'user' | 'admin'
 }
 
-export const NavItems = ({ page, lang, mode = 'user' }: PropTypes) =>
-  NAV_MENU().map((item) => (
+export const NavItems = ({ page, lang, mode = 'user' }: PropTypes) => {
+  if (mode === 'user') {
+    return NAV_MENU().map((item) => (
+      <NavItem
+        key={item.slug}
+        item={item}
+        page={page}
+        lang={lang}
+        mode={mode}
+      />
+    ))
+  }
+  return NAV_MENU_ADMIN().map((item) => (
     <NavItem key={item.slug} item={item} page={page} lang={lang} mode={mode} />
   ))
+}
