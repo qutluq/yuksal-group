46a13fcfb1b328882c1d7f4917052256ba32665b
qutluq <kootlook.applications@gmail.com>
2023-10-22 06:18:42 +0600

    chore: create about main table

---
+++ prisma/migrations/20231022001632_about_main/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "AboutMain" (
+    "id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "AboutMain_pkey" PRIMARY KEY ("id")
+);
